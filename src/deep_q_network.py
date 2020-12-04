af08e04f01ffd2ff8460872f0363350dc5b983e9
SophiaCheung1211 <46754180+SophiaCheung1211@users.noreply.github.com>
2020-12-04 14:48:54 -0500

    Add files via upload

---
+++ src/deep_q_network.py
@@ -0,0 +1,36 @@
+from torchsummary import summary
+
+import torch.nn as nn
+
+class DeepQNetwork(nn.Module):
+    def __init__(self):
+        super(DeepQNetwork, self).__init__()
+        
+        # changed the first layer when we need to change the number of heuristics we used to train the agent
+        self.conv1 = nn.Sequential(nn.Linear(5, 64), nn.ReLU(inplace=True))
+        self.conv2 = nn.Sequential(nn.Linear(64, 64), nn.ReLU(inplace=True))
+        self.conv3 = nn.Sequential(nn.Linear(64, 1))
+
+        self._create_weights()
+
+    def _create_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Linear):
+                nn.init.xavier_uniform_(m.weight)
+                nn.init.constant_(m.bias, 0)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+
+        return x
+
+model = DeepQNetwork()
+summary(model)
+
+
+        
+            
+        
+        
